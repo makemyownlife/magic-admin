480f84b8b2d563a81111382840f513042aa5bfd9
zhangyong <zhangyong7120180@163.com>
2025-04-08 14:26:47 +0800

    first init ruoyi-vue-pro

--- magic-common/magic-common-core/src/main/java/cn/javayong/magic/common/core/validate/AddGroup.java
+++
@@ -1,9 +0,0 @@
-package cn.javayong.magic.common.core.validate;
-
-/**
- * 校验分组 add
- *
- * @author Lion Li
- */
-public interface AddGroup {
-}
--- magic-common/magic-common-core/src/main/java/cn/javayong/magic/common/core/validate/EditGroup.java
+++
@@ -1,9 +0,0 @@
-package cn.javayong.magic.common.core.validate;
-
-/**
- * 校验分组 edit
- *
- * @author Lion Li
- */
-public interface EditGroup {
-}
