b373e5afb554c48878402bc9bc2af2ba7b053c39
zhangyong <zhangyong7120180@163.com>
2025-04-08 14:27:56 +0800

    提交最简模块

---
+++ yudao-module-infra/yudao-module-infra-biz/target/generated-sources/annotations/cn/iocoder/yudao/module/infra/convert/redis/RedisConvertImpl.java
@@ -0,0 +1,11 @@
+package cn.iocoder.yudao.module.infra.convert.redis;
+
+import javax.annotation.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-04-08T14:24:37+0800",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.4 (Oracle Corporation)"
+)
+public class RedisConvertImpl implements RedisConvert {
+}
---
+++ yudao-module-system/yudao-module-system-biz/target/generated-sources/annotations/cn/iocoder/yudao/module/system/convert/oauth2/OAuth2OpenConvertImpl.java
@@ -0,0 +1,11 @@
+package cn.iocoder.yudao.module.system.convert.oauth2;
+
+import javax.annotation.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-04-08T14:24:18+0800",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.4 (Oracle Corporation)"
+)
+public class OAuth2OpenConvertImpl implements OAuth2OpenConvert {
+}
