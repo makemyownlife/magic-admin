ddd1e01b54dd37b93de65d7fd4b107366b06f1b4
zhangyong <zhangyong7120180@163.com>
2025-04-13 12:33:18 +0800

    默认控制器

--- magic-server/src/main/java/cn/javayong/magic/server/controller/DefaultController.java
+++ magic-server/src/main/java/cn/javayong/magic/server/controller/DefaultController.java
@@ -21,62 +21,6 @@ import static cn.javayong.magic.framework.common.exception.enums.GlobalErrorCode
 @Slf4j
 public class DefaultController {
 
-    @RequestMapping("/admin-api/bpm/**")
-    public CommonResult<Boolean> bpm404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[工作流模块 magic-module-bpm - 已禁用]");
-    }
-
-    @RequestMapping("/admin-api/mp/**")
-    public CommonResult<Boolean> mp404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[微信公众号 magic-module-mp - 已禁用]");
-    }
-
-    @RequestMapping(value = { "/admin-api/product/**", // 商品中心
-            "/admin-api/trade/**", // 交易中心
-            "/admin-api/promotion/**" }) // 营销中心
-    public CommonResult<Boolean> mall404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[商城系统 magic-module-mall - 已禁用]");
-    }
-
-    @RequestMapping("/admin-api/erp/**")
-    public CommonResult<Boolean> erp404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[ERP 模块 magic-module-erp - 已禁用]");
-    }
-
-    @RequestMapping("/admin-api/crm/**")
-    public CommonResult<Boolean> crm404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[CRM 模块 magic-module-crm - 已禁用]");
-    }
-
-    @RequestMapping(value = { "/admin-api/report/**"})
-    public CommonResult<Boolean> report404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[报表模块 magic-module-report - 已禁用]");
-    }
-
-    @RequestMapping(value = { "/admin-api/pay/**"})
-    public CommonResult<Boolean> pay404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[支付模块 magic-module-pay - 已禁用]");
-    }
-
-    @RequestMapping(value = { "/admin-api/ai/**"})
-    public CommonResult<Boolean> ai404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[AI 大模型 magic-module-ai - 已禁用]");
-    }
-
-    @RequestMapping(value = { "/admin-api/iot/**"})
-    public CommonResult<Boolean> iot404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[IoT 物联网 magic-module-iot - 已禁用]");
-    }
-
     /**
      * 测试接口：打印 query、header、body
      */
