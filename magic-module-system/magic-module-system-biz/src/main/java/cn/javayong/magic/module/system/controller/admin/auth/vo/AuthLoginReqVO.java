9acd1f247171908dad6f86c7492f317133791b13
zhangyong <zhangyong7120180@163.com>
2025-04-18 15:45:24 +0800

    用户名密码登录对象

--- magic-module-system/magic-module-system-biz/src/main/java/cn/javayong/magic/module/system/controller/admin/auth/vo/AuthLoginReqVO.java
+++ magic-module-system/magic-module-system-biz/src/main/java/cn/javayong/magic/module/system/controller/admin/auth/vo/AuthLoginReqVO.java
@@ -21,13 +21,13 @@ import javax.validation.constraints.Pattern;
 @Builder
 public class AuthLoginReqVO extends CaptchaVerificationReqVO {
 
-    @Schema(description = "账号", requiredMode = Schema.RequiredMode.REQUIRED, example = "magicyuanma")
+    @Schema(description = "账号", requiredMode = Schema.RequiredMode.REQUIRED, example = "admin")
     @NotEmpty(message = "登录账号不能为空")
     @Length(min = 4, max = 16, message = "账号长度为 4-16 位")
     @Pattern(regexp = "^[A-Za-z0-9]+$", message = "账号格式为数字以及字母")
     private String username;
 
-    @Schema(description = "密码", requiredMode = Schema.RequiredMode.REQUIRED, example = "buzhidao")
+    @Schema(description = "密码", requiredMode = Schema.RequiredMode.REQUIRED, example = "admin123")
     @NotEmpty(message = "密码不能为空")
     @Length(min = 4, max = 16, message = "密码长度为 4-16 位")
     private String password;
