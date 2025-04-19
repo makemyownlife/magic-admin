705cc5e21637f59960dd1e7ce73abc3b32b96b06
zhangyong <zhangyong7120180@163.com>
2025-04-19 20:34:14 +0800

    去掉 TEST 模块

--- magic-framework/magic-spring-boot-starter-token/src/main/java/cn/javayong/magic/framework/token/config/MagicTokenAutoConfiguration.java
+++ magic-framework/magic-spring-boot-starter-token/src/main/java/cn/javayong/magic/framework/token/config/MagicTokenAutoConfiguration.java
@@ -1,11 +1,16 @@
 package cn.javayong.magic.framework.token.config;
 
 import cn.javayong.magic.framework.redis.config.MagicRedisAutoConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
 
 @AutoConfiguration(after = MagicRedisAutoConfiguration.class)
 public class MagicTokenAutoConfiguration {
 
-
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    
 
 }
---
+++ magic-framework/magic-spring-boot-starter-token/src/main/java/cn/javayong/magic/framework/token/core/SecurityTokenService.java
@@ -0,0 +1,7 @@
+package cn.javayong.magic.framework.token.core;
+
+public class SecurityTokenService {
+
+
+
+}
