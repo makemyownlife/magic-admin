e7df804a434cda65e6f3ea9e246acb62d54c9c52
zhangyong <zhangyong7120180@163.com>
2025-04-20 20:00:35 +0800

    安全访问 token 机制

--- magic-framework/magic-spring-boot-starter-token/src/main/java/cn/javayong/magic/framework/token/core/service/SecurityTokenService.java
+++ magic-framework/magic-spring-boot-starter-token/src/main/java/cn/javayong/magic/framework/token/core/service/SecurityTokenService.java
@@ -1,6 +1,10 @@
 package cn.javayong.magic.framework.token.core.service;
 
+import cn.javayong.magic.framework.common.exception.enums.GlobalErrorCodeConstants;
+import cn.javayong.magic.framework.common.util.date.DateUtils;
 import cn.javayong.magic.framework.token.core.dto.SecurityAccessTokenDTO;
+
+import static cn.javayong.magic.framework.common.exception.util.ServiceExceptionUtil.exception0;
 
 public interface SecurityTokenService {
 
@@ -8,6 +12,10 @@ public interface SecurityTokenService {
 
     SecurityAccessTokenDTO refreshAccessToken(String refreshToken);
 
-    void removeAccessToken(String accessToken);
+    SecurityAccessTokenDTO removeAccessToken(String accessToken);
+
+    SecurityAccessTokenDTO getAccessToken(String accessToken);
+
+    SecurityAccessTokenDTO checkAccessToken(String accessToken);
 
 }
--- magic-framework/magic-spring-boot-starter-token/src/main/java/cn/javayong/magic/framework/token/core/service/impl/SecurityTokenServiceImpl.java
+++ magic-framework/magic-spring-boot-starter-token/src/main/java/cn/javayong/magic/framework/token/core/service/impl/SecurityTokenServiceImpl.java
@@ -1,8 +1,12 @@
 package cn.javayong.magic.framework.token.core.service.impl;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.date.LocalDateTimeUtil;
 import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.ObjectUtil;
 import cn.javayong.magic.framework.common.enums.UserTypeEnum;
+import cn.javayong.magic.framework.common.exception.enums.GlobalErrorCodeConstants;
+import cn.javayong.magic.framework.common.util.date.DateUtils;
 import cn.javayong.magic.framework.common.util.json.JsonUtils;
 import cn.javayong.magic.framework.token.core.dto.SecurityAccessTokenDTO;
 import cn.javayong.magic.framework.token.core.dto.SecurityRefreshTokenDTO;
@@ -11,8 +15,10 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.Collections;
 import java.util.concurrent.TimeUnit;
+
+import static cn.javayong.magic.framework.common.exception.util.ServiceExceptionUtil.exception0;
+import static cn.javayong.magic.framework.common.util.collection.CollectionUtils.convertSet;
 
 public class SecurityTokenServiceImpl implements SecurityTokenService {
 
@@ -29,6 +35,7 @@ public class SecurityTokenServiceImpl implements SecurityTokenService {
     }
 
     //=========================================== 核心方法 start ===================================================
+    @Override
     public SecurityAccessTokenDTO createAccessToken(Long userId, Long tenantId) {
         // step1 ：创建 refreshToken
         SecurityRefreshTokenDTO refreshTokenDTO = new SecurityRefreshTokenDTO().setRefreshToken(generateRefreshToken())
@@ -52,7 +59,7 @@ public class SecurityTokenServiceImpl implements SecurityTokenService {
 
         // step4 : 保存 accessToken 到 Redis
         String accessTokenKey = String.format(SECURITY_ACCESS_TOKEN, accessTokenDTO.getAccessToken());
-        long timeDiff2 = LocalDateTimeUtil.between(LocalDateTime.now(), refreshTokenDTO.getExpiresTime(), ChronoUnit.SECONDS);
+        long timeDiff2 = LocalDateTimeUtil.between(LocalDateTime.now(), accessTokenDTO.getExpiresTime(), ChronoUnit.SECONDS);
         if (timeDiff2 > 0) {
             stringRedisTemplate.opsForValue().set(accessTokenKey, JsonUtils.toJsonString(accessTokenDTO), timeDiff2, TimeUnit.SECONDS);
         }
@@ -60,12 +67,84 @@ public class SecurityTokenServiceImpl implements SecurityTokenService {
         return accessTokenDTO;
     }
 
+    @Override
     public SecurityAccessTokenDTO refreshAccessToken(String refreshToken) {
-        return null;
+        // step1:从 Redis 查看刷新令牌
+        String refreshTokenKey = String.format(SECURITY_REFRESH_TOKEN, refreshToken);
+        SecurityAccessTokenDTO refreshTokenDTO =
+                JsonUtils.parseObject(
+                        stringRedisTemplate.opsForValue().get(refreshTokenKey),
+                        SecurityAccessTokenDTO.class);
+        if (refreshTokenDTO == null) {
+            throw exception0(GlobalErrorCodeConstants.BAD_REQUEST.getCode(), "无效的刷新令牌");
+        }
+
+        // step2:已过期的情况下，删除刷新令牌
+        if (DateUtils.isExpired(refreshTokenDTO.getExpiresTime())) {
+            stringRedisTemplate.delete(refreshTokenKey);
+            throw exception0(GlobalErrorCodeConstants.UNAUTHORIZED.getCode(), "刷新令牌已过期");
+        }
+
+        // step3 ：创建 accessToken
+        SecurityAccessTokenDTO accessTokenDTO = new SecurityAccessTokenDTO().setAccessToken(generateAccessToken())
+                .setUserId(refreshTokenDTO.getUserId()).setUserType(refreshTokenDTO.getUserType())
+                .setClientId(refreshTokenDTO.getClientId())
+                .setRefreshToken(refreshTokenDTO.getRefreshToken())
+                .setExpiresTime(LocalDateTime.now().plusSeconds(1800)).setTenantId(refreshTokenDTO.getTenantId());
+
+        // step4 : 保存 accessToken 到 Redis
+        String accessTokenKey = String.format(SECURITY_ACCESS_TOKEN, accessTokenDTO.getAccessToken());
+        long timeDiff2 = LocalDateTimeUtil.between(LocalDateTime.now(), accessTokenDTO.getExpiresTime(), ChronoUnit.SECONDS);
+        if (timeDiff2 > 0) {
+            stringRedisTemplate.opsForValue().set(accessTokenKey, JsonUtils.toJsonString(accessTokenDTO), timeDiff2, TimeUnit.SECONDS);
+        }
+
+        return accessTokenDTO;
     }
 
-    public void removeAccessToken(String accessToken) {
+    @Override
+    public SecurityAccessTokenDTO removeAccessToken(String accessToken) {
+        // step1:从 Redis 获取 访问令牌对象
+        String accessTokenKey = String.format(SECURITY_ACCESS_TOKEN, accessToken);
+        SecurityAccessTokenDTO securityAccessTokenDTO = JsonUtils.parseObject(
+                stringRedisTemplate.opsForValue().get(accessTokenKey),
+                SecurityAccessTokenDTO.class);
+        if (securityAccessTokenDTO == null) {
+            return null;
+        }
 
+        // step2:从 Redis 删除访问令牌
+        stringRedisTemplate.delete(accessTokenKey);
+
+        // step3:从 Redis 删除刷新令牌
+        String refreshTokenKey = String.format(SECURITY_REFRESH_TOKEN, securityAccessTokenDTO.getRefreshToken());
+        stringRedisTemplate.delete(refreshTokenKey);
+
+        return securityAccessTokenDTO;
+    }
+
+    @Override
+    public SecurityAccessTokenDTO getAccessToken(String accessToken) {
+        String accessTokenKey = String.format(SECURITY_ACCESS_TOKEN, accessToken);
+        SecurityAccessTokenDTO securityAccessTokenDTO = JsonUtils.parseObject(
+                stringRedisTemplate.opsForValue().get(accessTokenKey),
+                SecurityAccessTokenDTO.class);
+        if (securityAccessTokenDTO == null) {
+            return null;
+        }
+        return securityAccessTokenDTO;
+    }
+
+    @Override
+    public SecurityAccessTokenDTO checkAccessToken(String accessToken) {
+        SecurityAccessTokenDTO accessTokenDTO = getAccessToken(accessToken);
+        if (accessTokenDTO == null) {
+            throw exception0(GlobalErrorCodeConstants.UNAUTHORIZED.getCode(), "访问令牌不存在");
+        }
+        if (DateUtils.isExpired(accessTokenDTO.getExpiresTime())) {
+            throw exception0(GlobalErrorCodeConstants.UNAUTHORIZED.getCode(), "访问令牌已过期");
+        }
+        return accessTokenDTO;
     }
 
     //=========================================== 核心方法 end  ===========================================
