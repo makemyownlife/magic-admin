6e62fbaf422ecc8d2a5c7ab8cc4e5774a38e4f58
zhangyong <zhangyong7120180@163.com>
2025-04-30 19:37:35 +0800

    sse 的两种方式  sseemitor 和 webFlux

--- magic-module-ai/src/main/java/cn/javayong/magic/module/ai/controller/AppChatController.java
+++ magic-module-ai/src/main/java/cn/javayong/magic/module/ai/controller/AppChatController.java
@@ -5,13 +5,16 @@ import cn.javayong.magic.framework.common.util.servlet.ServletUtils;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import reactor.core.publisher.Flux;
 
 import javax.annotation.security.PermitAll;
 import javax.servlet.http.HttpServletRequest;
+import java.time.Duration;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -27,23 +30,43 @@ public class AppChatController {
     /**
      * 简单的 SSE 代码例子
      */
-    @RequestMapping(value = "/chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @RequestMapping(value = "/chatDemo1", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     @PermitAll
-    public SseEmitter chatDemo() {
+    public SseEmitter chatDemo12() {
         SseEmitter emitter = new SseEmitter();
-        try {
-            for (int i = 0; i < 10; i++) {
-                SseEmitter.SseEventBuilder event = SseEmitter.event()
-                        .name("message")
-                        .data("SSE消息 " + i);
-                emitter.send(event);
-                Thread.sleep(1000);
+
+        // 此处必须是线程池，否则页面会阻塞
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < 10; i++) {
+                        SseEmitter.SseEventBuilder event = SseEmitter.event()
+                                .name("message")
+                                .data("SSE消息 " + i);
+                        emitter.send(event);
+                        Thread.sleep(1000);
+                    }
+                    // 最后调用 complete 方法
+                    emitter.complete();
+                } catch (Exception e) {
+                    emitter.completeWithError(e);
+                }
             }
-            emitter.complete();
-        } catch (Exception e) {
-            emitter.completeWithError(e);
-        }
+        });
+
         return emitter;
+    }
+
+    @RequestMapping(value = "/chatDemo2", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @PermitAll
+    public Flux<ServerSentEvent<String>> streamCustomEvents() {
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence -> ServerSentEvent.<String>builder()
+                        .id(String.valueOf(sequence))
+                        .event("custom-event")
+                        .data("Event #" + sequence)
+                        .build());
     }
 
 }
