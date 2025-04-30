c67409ae7713056fc81fdf28fb86cd6c941dd9fd
zhangyong <zhangyong7120180@163.com>
2025-04-30 16:33:49 +0800

    添加 管理层 、用户层 控制器

---
+++ magic-module-ai/src/main/java/cn/javayong/magic/module/ai/controller/AdminAIController.java
@@ -0,0 +1,14 @@
+package cn.javayong.magic.module.ai.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "管理后台 - 验证码")
+@RestController("AdminAIController")
+@RequestMapping("/ai/admin")
+public class AdminAIController {
+
+
+
+}
---
+++ magic-module-ai/src/main/java/cn/javayong/magic/module/ai/controller/AppChatController.java
@@ -0,0 +1,14 @@
+package cn.javayong.magic.module.ai.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "用户端-智能聊天")
+@RestController("AppChatController")
+@RequestMapping("/ai/app")
+public class AppChatController {
+
+
+
+}
