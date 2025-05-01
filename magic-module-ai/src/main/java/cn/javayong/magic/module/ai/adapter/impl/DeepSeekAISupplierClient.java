cb1b36715586b169ea69a1b6d8128463ec11cd6e
zhangyong <zhangyong7120180@163.com>
2025-05-01 19:59:20 +0800

    优化 POM 文件

--- magic-module-ai/src/main/java/cn/javayong/magic/module/ai/adapter/impl/DeepSeekAISupplierClient.java
+++ magic-module-ai/src/main/java/cn/javayong/magic/module/ai/adapter/impl/DeepSeekAISupplierClient.java
@@ -2,11 +2,16 @@ package cn.javayong.magic.module.ai.adapter.impl;
 
 import cn.javayong.magic.module.ai.adapter.AISupplierClient;
 import cn.javayong.magic.module.ai.adapter.AISupplierConfig;
+import reactor.core.publisher.Flux;
 
 /**
  * 标准 DeepSeek 供应商 API ，兼容 openai 协议
  */
 public class DeepSeekAISupplierClient implements AISupplierClient {
+
+    final static String apiUrl = "https://api.deepseek.com/v1/chat/completions";
+
+    final static String apiKey = "sk-31da87a7c6eb40188fb1a71f98fa6fbd";
 
     private AISupplierConfig aiSupplierConfig;
 
@@ -15,8 +20,10 @@ public class DeepSeekAISupplierClient implements AISupplierClient {
         this.aiSupplierConfig = aiSupplierConfig;
     }
 
+
     @Override
     public void destroy() {
+
     }
 
 }
