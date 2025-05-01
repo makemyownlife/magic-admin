cfd1470dfd569af669085924ed3449134634f55f
zhangyong <zhangyong7120180@163.com>
2025-05-01 23:56:34 +0800

    openai 接口

--- magic-module-ai/src/main/java/cn/javayong/magic/module/ai/controller/OpenAIController.java
+++ magic-module-ai/src/main/java/cn/javayong/magic/module/ai/controller/OpenAIController.java
@@ -1,5 +1,7 @@
 package cn.javayong.magic.module.ai.controller;
 
+import cn.javayong.magic.module.ai.adapter.AISupplierClient;
+import cn.javayong.magic.module.ai.adapter.impl.DeepSeekAISupplierClient;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -11,25 +13,21 @@ import reactor.core.publisher.Flux;
 import javax.annotation.security.PermitAll;
 import java.time.Duration;
 
-@Tag(name = "管理后台 - 验证码")
+@Tag(name = "兼容  openai 的核心接口")
 @RestController("OpenAIController")
-@RequestMapping("/ai/admin")
+@RequestMapping("/chat")
 @Slf4j
 public class OpenAIController {
 
     /**
      * 创建文本对话请求 参考 硅基流动  https://docs.siliconflow.cn/cn/api-reference/chat-completions/chat-completions
      */
-    @RequestMapping(value = "/chat/completions", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @RequestMapping(value = "/completions", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     @PermitAll
     public Flux<ServerSentEvent<String>> streamCustomEvents() {
-        return Flux.interval(Duration.ofSeconds(1))
-                .map(sequence -> ServerSentEvent.<String>builder()
-                        .id(String.valueOf(sequence))
-                        .event("custom-event")
-                        .data("Event #" + sequence)
-                        .build());
+        AISupplierClient aiSupplierClient = new DeepSeekAISupplierClient();
+        Flux<String> result = aiSupplierClient.chatCompletion();
+        return result.map(data -> ServerSentEvent.builder(data).build());
     }
-    
 
 }
