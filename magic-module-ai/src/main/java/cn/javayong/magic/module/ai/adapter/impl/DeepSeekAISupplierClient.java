1919f7aa2aa9f6af1350f5e03024ad279d747d81
zhangyong <zhangyong7120180@163.com>
2025-05-01 16:48:58 +0800

    标准 DeepSeek 供应商 API ，兼容 openai 协议

--- magic-module-ai/src/main/java/cn/javayong/magic/module/ai/adapter/impl/DeepSeekAISupplierClient.java
+++ magic-module-ai/src/main/java/cn/javayong/magic/module/ai/adapter/impl/DeepSeekAISupplierClient.java
@@ -4,7 +4,7 @@ import cn.javayong.magic.module.ai.adapter.AISupplierClient;
 import cn.javayong.magic.module.ai.adapter.AISupplierConfig;
 
 /**
- * 标准 DeepSeek 供应商 API 
+ * 标准 DeepSeek 供应商 API ，兼容 openai 协议
  */
 public class DeepSeekAISupplierClient implements AISupplierClient {
 
