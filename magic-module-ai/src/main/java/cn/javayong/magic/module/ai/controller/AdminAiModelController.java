ac6c6701c63f8a31b39e11df518e572704f6c871
zhangyong <zhangyong7120180@163.com>
2025-05-08 23:36:01 +0800

    添加分页列表

--- magic-module-ai/src/main/java/cn/javayong/magic/module/ai/controller/AdminAiModelController.java
+++ magic-module-ai/src/main/java/cn/javayong/magic/module/ai/controller/AdminAiModelController.java
@@ -1,9 +1,20 @@
 package cn.javayong.magic.module.ai.controller;
 
+import cn.javayong.magic.framework.common.pojo.CommonResult;
+import cn.javayong.magic.framework.common.pojo.PageResult;
+import cn.javayong.magic.framework.common.util.object.BeanUtils;
+import cn.javayong.magic.module.ai.domain.vo.AiModelPageReqVO;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+import static cn.javayong.magic.framework.common.pojo.CommonResult.success;
 
 @Tag(name = "后台AI管理-模型配置")
 @RestController("AdminAIController")
@@ -11,6 +22,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class AdminAiModelController {
 
-
+    @GetMapping("/page")
+    @Operation(summary = "获得模型分页")
+    @PreAuthorize("@ss.hasPermission('ai:model:query')")
+    public CommonResult<PageResult> getModelPage(@Valid AiModelPageReqVO pageReqVO) {
+        PageResult pageResult = PageResult.empty();
+        return success(pageResult);
+    }
 
 }
