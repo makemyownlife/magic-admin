31a214ca7a8f440ff6fba75125644b0ebd1f599e
zhangyong <zhangyong7120180@163.com>
2025-05-08 15:13:32 +0800

    添加模型配置 menu

--- magic-module-ai/src/main/java/cn/javayong/magic/module/ai/controller/AdminAiModelController.java
+++ magic-module-ai/src/main/java/cn/javayong/magic/module/ai/controller/AdminAiModelController.java
@@ -1,13 +1,15 @@
 package cn.javayong.magic.module.ai.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "后台AI管理")
+@Tag(name = "后台AI管理-模型配置")
 @RestController("AdminAIController")
-@RequestMapping("/ai/admin")
-public class AdminAIController {
+@RequestMapping("/ai/model/")
+@Slf4j
+public class AdminAiModelController {
 
 
 
