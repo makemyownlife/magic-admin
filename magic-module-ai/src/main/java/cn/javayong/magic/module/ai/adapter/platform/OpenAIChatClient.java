0760240c40d234665a44fa83e946f0452006d566
zhangyong <zhangyong7120180@163.com>
2025-05-16 16:00:42 +0800

    添加标准 OpenAI 对话客户端

--- magic-module-ai/src/main/java/cn/javayong/magic/module/ai/adapter/platform/OpenAIChatClient.java
+++ magic-module-ai/src/main/java/cn/javayong/magic/module/ai/adapter/platform/OpenAIChatClient.java
@@ -1,4 +1,21 @@
 package cn.javayong.magic.module.ai.adapter.platform;
 
-public class OpenAIChatClient {
+import cn.javayong.magic.module.ai.adapter.command.OpenAIChatCompletions;
+import cn.javayong.magic.module.ai.adapter.command.OpenAIChatReqCommand;
+import cn.javayong.magic.module.ai.adapter.command.OpenAIChatRespCommand;
+import cn.javayong.magic.module.ai.adapter.core.AiPlatformChatClient;
+import reactor.core.publisher.Flux;
+
+public class OpenAIChatClient implements AiPlatformChatClient {
+
+    @Override
+    public OpenAIChatRespCommand<Flux<String>> streamChatCompletion(OpenAIChatReqCommand openAIChatReqCommand) {
+        return null;
+    }
+
+    @Override
+    public OpenAIChatRespCommand<OpenAIChatCompletions> blockChatCompletion(OpenAIChatReqCommand openAIChatReqCommand) {
+        return null;
+    }
+
 }
