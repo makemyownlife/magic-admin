39651b6b6a1b67f967cbfd449c77609403b1505b
zhangyong <zhangyong7120180@163.com>
2025-05-17 18:20:37 +0800

    AI OneAPI 令牌实体类

--- magic-module-ai/src/main/java/cn/javayong/magic/module/ai/controller/AdminAiTokenContoller.java
+++ magic-module-ai/src/main/java/cn/javayong/magic/module/ai/controller/AdminAiTokenContoller.java
@@ -16,5 +16,4 @@ public class AdminAiTokenContoller {
 
 
 
-
 }
---
+++ magic-module-ai/src/main/java/cn/javayong/magic/module/ai/domain/AiOneApiTokenDO.java
@@ -0,0 +1,51 @@
+package cn.javayong.magic.module.ai.domain;
+
+import cn.javayong.magic.framework.mybatis.core.dataobject.BaseDO;
+import com.baomidou.mybatisplus.annotation.KeySequence;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+/**
+ * AI OneAPI 令牌实体类
+ */
+@TableName("ai_oneapi_token")
+@KeySequence("ai_oneapi_token_seq") // 用于 Oracle、PostgreSQL、Kingbase、DB2、H2 数据库的主键自增
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class AiOneApiTokenDO extends BaseDO {
+
+    /**
+     * 主键ID (雪花算法)
+     */
+    private Long id;
+
+    /**
+     *  模型列表
+     */
+    private String modelIds;
+
+    /**
+     * 令牌名称
+     */
+    private String name;
+
+    /**
+     * 令牌值 (实际API密钥)
+     */
+    private String token;
+
+    /**
+     * 过期时间
+     */
+    private LocalDateTime expireTime;
+
+
+}
