49e15d2b52dee311bd87541293f70af5ec084735
zhangyong <zhangyong7120180@163.com>
2025-05-19 17:17:04 +0800

    兼容 openai 的接口协议 , 提取 token 值

--- magic-module-ai/src/main/java/cn/javayong/magic/module/ai/controller/OpenAIController.java
+++ magic-module-ai/src/main/java/cn/javayong/magic/module/ai/controller/OpenAIController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,10 +30,10 @@ public class OpenAIController {
     @RequestMapping(value = "/completions",
             produces = {MediaType.TEXT_EVENT_STREAM_VALUE, MediaType.APPLICATION_JSON_VALUE})
     @PermitAll
-    public Object completions(@RequestBody OpenAIChatReqVO openAIChatReqVO) {
-        log.info("openAIChatReqVO:" + JsonUtils.toJsonString(openAIChatReqVO));
+    public Object completions(@RequestBody OpenAIChatReqVO openAIChatReqVO,
+                              @RequestHeader(value = "Authorization", required = false) String oneApiToken) {
+        log.info("openAIChatReqVO:" + JsonUtils.toJsonString(openAIChatReqVO) + " oneApiToken:" + oneApiToken);
         return openAIService.completions(openAIChatReqVO);
     }
-
 
 }
