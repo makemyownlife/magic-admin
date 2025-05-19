a467243626352b46aefd1c8dc45b195f90f32dec
zhangyong <zhangyong7120180@163.com>
2025-05-19 14:32:21 +0800

    oneapi token 分页 创建

--- magic-module-ai/src/main/java/cn/javayong/magic/module/ai/controller/AdminOneApiTokenContoller.java
+++ magic-module-ai/src/main/java/cn/javayong/magic/module/ai/controller/AdminOneApiTokenContoller.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
-
 import static cn.javayong.magic.framework.common.pojo.CommonResult.success;
 
 /**
--- magic-module-ai/src/main/java/cn/javayong/magic/module/ai/service/impl/AiOneApiTokenServiceImpl.java
+++ magic-module-ai/src/main/java/cn/javayong/magic/module/ai/service/impl/AiOneApiTokenServiceImpl.java
@@ -2,6 +2,7 @@ package cn.javayong.magic.module.ai.service.impl;
 
 import cn.javayong.magic.framework.common.pojo.PageResult;
 import cn.javayong.magic.framework.common.util.object.BeanUtils;
+import cn.javayong.magic.idgenerator.core.service.IdGeneratorService;
 import cn.javayong.magic.module.ai.domain.AiOneApiTokenDO;
 import cn.javayong.magic.module.ai.domain.vo.AiOneApiTokenPageReqVO;
 import cn.javayong.magic.module.ai.domain.vo.AiOneApiTokenSaveReqVO;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 import javax.annotation.Resource;
+import java.util.UUID;
 
 /**
  * one API token 配置 实现类
@@ -22,6 +24,9 @@ public class AiOneApiTokenServiceImpl implements AiOneApiTokenService {
     @Resource
     private AiOneApiTokenMapper oneApiTokenMapper;
 
+    @Resource
+    private IdGeneratorService idGeneratorService;
+
     @Override
     public PageResult<AiOneApiTokenDO> getOneApiTokenPage(AiOneApiTokenPageReqVO pageReqVO) {
         return oneApiTokenMapper.selectPage(pageReqVO);
@@ -29,9 +34,19 @@ public class AiOneApiTokenServiceImpl implements AiOneApiTokenService {
 
     @Override
     public Long createOneApiToken(AiOneApiTokenSaveReqVO createReqVO) {
+
         AiOneApiTokenDO oneApiTokenDO = BeanUtils.toBean(createReqVO, AiOneApiTokenDO.class);
+
+        // 通过 ID 生成器生成唯一 ID
+        oneApiTokenDO.setId(idGeneratorService.createUniqueId("ai_oneapi_token"));
+
+        // 通过 UUID 生成唯一 token
+        oneApiTokenDO.setToken(UUID.randomUUID().toString());
+
         oneApiTokenDO.setDeleted(false);
+
         oneApiTokenMapper.insert(oneApiTokenDO);
+
         return oneApiTokenDO.getId();
     }
 
