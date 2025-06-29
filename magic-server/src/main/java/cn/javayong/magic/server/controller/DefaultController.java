d21e4a0982fd93df39adbfc50812b06d932a953d
zhangyong <zhangyong7120180@163.com>
2025-06-29 16:52:08 +0800

    测试使用

--- magic-server/src/main/java/cn/javayong/magic/server/controller/DefaultController.java
+++ magic-server/src/main/java/cn/javayong/magic/server/controller/DefaultController.java
@@ -12,9 +12,7 @@ import javax.annotation.security.PermitAll;
 import javax.servlet.http.HttpServletRequest;
 
 /**
- * 默认 Controller，解决部分 module 未开启时的 404 提示。
- * 例如说，/bpm/** 路径，工作流
- *
+ * 默认 Controller 做测试用
  * @author zhangyong
  */
 @RestController
