5e3e45b6d4bd9c691c216a1192c02d2afaa34ce5
zhangyong <zhangyong7120180@163.com>
2025-07-16 17:27:31 +0800

    系统日志

--- magic-module-system/src/main/java/cn/javayong/magic/module/system/domain/dataobject/OperateLogDO.java
+++ magic-module-system/src/main/java/cn/javayong/magic/module/system/domain/dataobject/OperateLogDO.java
@@ -9,8 +9,6 @@ import lombok.Data;
 
 /**
  * 操作日志表
- *
-
  */
 @TableName(value = "system_operate_log", autoResultMap = true)
 @KeySequence("system_operate_log_seq") // 用于 Oracle、PostgreSQL、Kingbase、DB2、H2 数据库的主键自增。如果是 MySQL 等数据库，可不写。
